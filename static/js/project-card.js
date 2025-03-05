1eb9466dc8bcc73d53bff0eebbff8a5a3409c0ea
BrunoCTmb <zazaduza10@gmail.com>
2025-03-05 01:03:59 -0300

    Update card content system => JS change all card content with dynamic way

--- static/js/project-card.js
+++ static/js/project-card.js
@@ -1,4 +1,30 @@
-function openCard() {
+const pj1Content = {
+    'title': 'Arena virtual', 
+    'eachTechs': ['Java', 'Spring Projects'], 
+    'pjImageUrls': ['images/not-found.png'], 
+    'pjDescription': 'Uma Plataforma de gerenciamento de campeonatos, sendo uma RestApi desenvolvida em Java com Spring para criação, organização e gerenciamento de campeonatos. Permite aos usuários criar competições personalizadas, gerenciar times e jogadores e facilitar torneios.', 
+    'pjGithubUrl': 'www.github.com/BrunoCTb/Arena-virtual'
+}
+
+const pj2Content = {
+    'title': 'Pets', 
+    'eachTech': ['tech1'], 
+    'pjImageUrl': ['urlimage'], 
+    'pjDescription': 'desc', 
+    'pjGithubUrl': 'http'
+}
+
+const pj3Content = {
+    'title': 'Safe Text', 
+    'eachTech': [], 
+    'pjImageUrl': [], 
+    'pjDescription': '', 
+    'pjGithubUrl': ''
+}
+
+const projectsList = [pj1Content, pj2Content, pj3Content]
+
+function openCard(projectTitle) {
     card = document.getElementsByClassName("project-card")[0]
     card.style.display = "block";
 
@@ -9,6 +35,11 @@ function openCard() {
 
     body = document.getElementsByTagName("body")[0]
     body.style.overflowY = "hidden";
+
+    let project = getProject(projectTitle);
+    let html = editPjContent(project)
+    
+    card.innerHTML += html;
 }
 
 function closeCard() {
@@ -24,3 +55,51 @@ function closeCard() {
     body.style.overflowY = "scroll";
 }
 
+function getProject(projectTitle) {
+    for (i of projectsList) {
+        if(i.title.toLowerCase().trim() == projectTitle.toLowerCase().trim()) {
+            return i;
+        }
+    }
+
+    return;
+}
+
+function editPjContent(data) {
+    // adicionar cada tech da lista do js como se fosse um html
+    loopPjTechs = ''
+    for (tech of data.eachTechs) {
+        loopPjTechs += `<span>${tech}</span>`
+    }
+        
+    // fazer o mesmo que acima, porem para a lista de url das imagens
+    loopPjImageUrl = ''
+    for (img of data.pjImageUrls) {
+        loopPjImageUrl += `<img src="${img}" alt="imagem">`
+    }
+    
+    return `<div class="close-card" onclick="closeCard()">
+                <span>x</span>
+            </div>
+            <div class="project-card-title">
+                <h2>${data.title}</h2>
+            </div>
+            <div class="project-card-techs">
+                ${loopPjTechs}
+            </div>
+            <div class="project-card-images">
+                <span id="previous-image"><</span>
+                <div class="project-card-carousel">
+                    ${loopPjImageUrl}
+                </div>
+                <span id="previous-image">></span>
+            </div>
+            <div class="description">
+                
+                <p>${data.pjDescription}</p>
+            </div>
+            <div class="btn-actions">
+                <a href="${data.pjGithubUrl}"><img src="images/social/github.png" alt="github">Código</a>
+                <button onclick="closeCard()">Fechar</button>
+            </div>`
+}
